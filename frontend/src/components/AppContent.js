816a889ad5faf0d06ffb2cc7dc27956fb380b350
user19522442 <19522442@gm.uit.edu.vn>
2023-07-05 01:11:27 +0700

    first commit

---
+++ frontend/src/components/AppContent.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import WelcomeContent from './WelcomeContent';
+
+export default class AppContent extends React.Component {
+
+  render() {
+    return (
+        <div>
+            <WelcomeContent/>
+        </div>
+    );
+  };
+}
